f8f992c8b28e51b84313df8a1c097a933666b3e6
Satyendra Singh More <satyendramore97@gmail.com>
2023-02-11 22:35:46 +0530

    onboarding added

--- App.js
+++ App.js
@@ -1,11 +1,188 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Animated,
+  Text,
+  Image,
+  View,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+} from "react-native";
+
+const { width, height } = Dimensions.get("screen");
+
+// https://www.flaticon.com/packs/retro-wave
+// inspiration: https://dribbble.com/shots/11164698-Onboarding-screens-animation
+// https://twitter.com/mironcatalin/status/1321180191935373312
+
+const bgs = ["#A5BBFF", "#DDBEFE", "#FF63ED", "#B98EFF"];
+const DATA = [
+  {
+    key: "3571572",
+    title: "Multi-lateral intermediate moratorium",
+    description:
+      "I'll back up the multi-byte XSS matrix, that should feed the SCSI application!",
+    image: require("./assets/images/onboarding1.jpg"),
+  },
+  {
+    key: "3571747",
+    title: "Automated radical data-warehouse",
+    description:
+      "Use the optical SAS system, then you can navigate the auxiliary alarm!",
+    image: require("./assets/images/onboarding2.jpg"),
+  },
+  {
+    key: "3571680",
+    title: "Inverse attitude-oriented system engine",
+    description:
+      "The ADP array is down, compress the online sensor so we can input the HTTP panel!",
+    image: require("./assets/images/onboarding3.jpg"),
+  },
+  {
+    key: "3571603",
+    title: "Monitored global data-warehouse",
+    description: "We need to program the open-source IB interface!",
+    image: require("./assets/images/onboarding4.jpg"),
+  },
+];
+
+const Indicator = ({ scrollx }) => {
+  return (
+    <View style={{ position: "absolute", bottom: 100, flexDirection: "row" }}>
+      {DATA.map((_, i) => {
+        const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+        const scale = scrollx.interpolate({
+          inputRange,
+          outputRange: [0.8, 1.4, 0.8],
+          extrapolate: "clamp",
+        });
+        const opacity = scrollx.interpolate({
+          inputRange,
+          outputRange: [0.6, 1, 0.6],
+          extrapolate: "clamp",
+        });
+        return (
+          <Animated.View
+            key={`indicator-${i}`}
+            style={{
+              height: 10,
+              width: 10,
+              borderRadius: 5,
+              backgroundColor: "#fff",
+              margin: 10,
+              opacity,
+              transform: [
+                {
+                  scale,
+                },
+              ],
+            }}
+          />
+        );
+      })}
+    </View>
+  );
+};
+
+const Backdrop = ({ scrollx }) => {
+  const backgroundColor = scrollx.interpolate({
+    inputRange: bgs.map((_, i) => i * width),
+    outputRange: bgs.map((bg) => bg),
+  });
+  return (
+    <Animated.View
+      style={[StyleSheet.absoluteFillObject, { backgroundColor }]}
+    />
+  );
+};
+
+const Square = ({ scrollx }) => {
+  const YOLO = Animated.modulo(
+    Animated.divide(Animated.modulo(scrollx, width), new Animated.Value(width)),
+    1
+  );
+
+  const rotate = YOLO.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: ["35deg", "0deg", "35deg"],
+  });
+  const translateX = YOLO.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [0, -height, 0],
+  });
+  return (
+    <Animated.View
+      style={{
+        width: height,
+        height: height,
+        backgroundColor: "#fff",
+        borderRadius: 86,
+        position: "absolute",
+        top: -height * 0.6,
+        transform: [
+          {
+            rotate,
+          },
+          {
+            translateX,
+          },
+        ],
+      }}
+    />
+  );
+};
 
 export default function App() {
+  const scrollx = React.useRef(new Animated.Value(0)).current;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      <Backdrop scrollx={scrollx} />
+      <Square scrollx={scrollx} />
+      <Animated.FlatList
+        data={DATA}
+        keyExtractor={(item) => item.key}
+        horizontal
+        pagingEnabled
+        scrollEventThrottle={32}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollx } } }],
+          { useNativeDriver: false }
+        )}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        renderItem={({ item }) => {
+          return (
+            <View style={{ width, alignItems: "center", padding: 20 }}>
+              <View
+                style={{
+                  flex: 0.7,
+                  justifyContent: "center",
+                }}
+              >
+                <Image
+                  source={item.image}
+                  style={{
+                    width: width / 2,
+                    height: width / 2,
+                    resizeMode: "contain",
+                  }}
+                />
+              </View>
+              <View>
+                <Text
+                  style={{ fontSize: 24, fontWeight: "800", marginBottom: 10 }}
+                >
+                  {item.title}
+                </Text>
+                <Text style={{ fontWeight: "300" }}>{item.description}</Text>
+              </View>
+            </View>
+          );
+        }}
+      />
+      <Indicator scrollx={scrollx} />
     </View>
   );
 }
@@ -13,8 +190,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
