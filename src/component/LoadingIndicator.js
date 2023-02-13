aabd83171cec707e1df83da28b6ce1ea58a98c8a
Satyendra Singh More <satyendramore97@gmail.com>
2023-02-13 21:07:04 +0530

    loading updated

--- src/component/LoadingIndicator.js
+++ src/component/LoadingIndicator.js
@@ -2,21 +2,40 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { MotiView } from "moti";
 
-const LoadingIndicator = () => {
+const LoadingIndicator = ({ size }) => {
   return (
     <MotiView
       from={{
-        opacity: 0,
-        scale: 0.5,
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        borderWidth: 0,
+        shadowOpacity: 0.5,
       }}
       animate={{
-        opacity: 1,
-        scale: 1,
+        width: size + 20,
+        height: size + 20,
+        borderRadius: (size + 20) / 2,
+        borderWidth: size / 10,
+        shadowOpacity: 1,
       }}
       transition={{
         type: "timing",
+        duration: 1000,
+        loop: true,
+        repeatReverse: true,
       }}
-      style={styles.shape}
+      style={{
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        borderWidth: size / 10,
+        borderColor: "#fff",
+        shadowColor: "#fff",
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+      }}
     />
   );
 };
--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -15,137 +15,71 @@ import LoadingIndicator from "../../component/LoadingIndicator";
 const { width, height } = Dimensions.get("screen");
 
 const Login = ({ navigation }) => {
-  const [visible, setVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const showLoading = () => {
+    setIsVisible(true);
+  };
 
   return (
     <View style={styles.container}>
-      <Image
-        source={loginImage}
-        style={{
-          width: 250,
-          height: 250,
-          resizeMode: "contain",
-          borderRadius: 15,
-          marginVertical: 40,
-        }}
-      />
-      {/* Welcome COntainer */}
-      <View style={{ alignSelf: "flex-start", marginHorizontal: 30 }}>
-        <Text
-          style={{ color: "white", fontFamily: "Roboto-Bold", fontSize: 22 }}
-        >
-          Let's Sign you in
-        </Text>
-        <Text style={{ color: "white", fontSize: 16, marginTop: 10 }}>
-          Welcome back.
-        </Text>
-        <Text style={{ color: "white", fontSize: 13 }}>
-          You've been missed!
-        </Text>
-      </View>
-
-      {visible && <LoadingIndicator />}
-      {/* Login Inputs */}
-      <View
-        style={{
-          marginVertical: 20,
-          width: 320,
-        }}
-      >
-        <View
-          style={{
-            borderWidth: 2,
-            borderColor: "grey",
-            borderRadius: 10,
-            paddingVertical: 5,
-            paddingHorizontal: 15,
-            marginBottom: 15,
-          }}
-        >
-          <TextInput
-            placeholder="Email"
-            placeholderTextColor="grey"
-            style={{ color: "white" }}
-          />
+      {isVisible ? (
+        <View style={[styles.container, { justifyContent: "center" }]}>
+          <LoadingIndicator size={100} />
         </View>
-        <View
-          style={{
-            borderWidth: 2,
-            borderColor: "grey",
-            borderRadius: 10,
-            paddingHorizontal: 15,
-            paddingVertical: 5,
-          }}
-        >
-          <TextInput
-            placeholder="pass"
-            placeholderTextColor="grey"
-            style={{ color: "white" }}
-          />
-        </View>
-      </View>
-
-      {/* Button containers */}
-      <View
-        style={{
-          position: "absolute",
-          bottom: 40,
-          width: 320,
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: 10,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 14,
-              color: "grey",
-              textAlign: "center",
-            }}
-          >
-            Wait! Are you NEW to app?
-          </Text>
-          <TouchableOpacity
-            style={{ alignItems: "center", justifyContent: "center" }}
-            onPress={() => {
-              navigation.navigate("Register");
-            }}
-          >
-            <Text style={{ fontSize: 14, color: "white", textAlign: "center" }}>
-              {"  "}Register
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <TouchableOpacity
-          onPress={() => {
-            setVisible(!visible);
-            console.log("button Pressed " + visible);
-          }}
-        >
-          <View
-            style={{
-              paddingVertical: 10,
-              backgroundColor: "white",
-              borderRadius: 2,
-            }}
-          >
-            <Text
-              style={{
-                fontFamily: "Roboto-Bold",
-                fontSize: 14,
-                textAlign: "center",
-              }}
-            >
-              Sign In
-            </Text>
+      ) : (
+        <>
+          <Image source={loginImage} style={styles.imgStyle} />
+          {/* Welcome COntainer */}
+          <View style={styles.welcomeContainer}>
+            <Text style={styles.signText}>Let's Sign you in</Text>
+            <Text style={styles.welcomeText}>Welcome back.</Text>
+            <Text style={styles.subText}>You've been missed!</Text>
           </View>
-        </TouchableOpacity>
-      </View>
+
+          {/* Login Inputs */}
+          <View style={styles.inputsContainer}>
+            <View style={styles.inputs}>
+              <TextInput
+                placeholder="Email"
+                placeholderTextColor="grey"
+                style={{ color: "white" }}
+              />
+            </View>
+            <View style={styles.inputs}>
+              <TextInput
+                placeholder="pass"
+                placeholderTextColor="grey"
+                style={{ color: "white" }}
+              />
+            </View>
+          </View>
+
+          {/* Button containers */}
+          <View style={styles.btnContainer}>
+            <View style={styles.registerContainer}>
+              <Text style={styles.registerTitle}>
+                Wait! Are you NEW to app?
+              </Text>
+              <TouchableOpacity
+                style={styles.registerBtn}
+                onPress={() => {
+                  navigation.navigate("Register");
+                }}
+              >
+                <Text style={styles.registerText}>{"  "}Register</Text>
+              </TouchableOpacity>
+            </View>
+
+            {/* Sign In btn */}
+            <TouchableOpacity onPress={showLoading}>
+              <View style={styles.signinBtnContainer}>
+                <Text style={styles.signBtnText}>Sign In</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </>
+      )}
     </View>
   );
 };
@@ -157,5 +91,58 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#000",
     alignItems: "center",
+    width: width,
+    height: height,
+  },
+  imgStyle: {
+    width: 250,
+    height: 250,
+    resizeMode: "contain",
+    borderRadius: 15,
+    marginVertical: 40,
+  },
+  welcomeContainer: { alignSelf: "flex-start", marginHorizontal: 30 },
+  signText: { color: "white", fontFamily: "Roboto-Bold", fontSize: 22 },
+  welcomeText: { color: "white", fontSize: 16, marginTop: 10 },
+  subText: { color: "white", fontSize: 13 },
+  inputsContainer: {
+    marginVertical: 20,
+    width: 320,
+  },
+  inputs: {
+    borderWidth: 2,
+    borderColor: "grey",
+    borderRadius: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+  },
+  btnContainer: {
+    position: "absolute",
+    bottom: 40,
+    width: 320,
+  },
+  registerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  registerTitle: {
+    fontSize: 14,
+    color: "grey",
+    textAlign: "center",
+  },
+  registerText: { fontSize: 14, color: "white", textAlign: "center" },
+  registerBtn: { alignItems: "center", justifyContent: "center" },
+  signinBtnContainer: {
+    paddingVertical: 10,
+    backgroundColor: "white",
+    borderRadius: 2,
+  },
+  signBtnText: {
+    fontFamily: "Roboto-Bold",
+    fontSize: 14,
+    textAlign: "center",
   },
 });
