a6f2d4db33d67e47cc8989da0e021c99180af2c5
Satyendra Singh More <satyendramore97@gmail.com>
2023-02-16 22:45:25 +0530

    api added

--- src/screens/app/Home.js
+++ src/screens/app/Home.js
@@ -5,16 +5,23 @@ import {
   View,
   TouchableOpacity,
   Dimensions,
+  ActivityIndicator,
 } from "react-native";
 import { MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
 import { AuthContext } from "../../navigation/AuthProvider";
 import { StatusBar } from "expo-status-bar";
+import axios from "axios";
 
 const { width, height } = Dimensions.get("screen");
 
 const Home = ({ navigation }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [isMessagePresent, setIsMessagePresent] = useState(false);
+  const [think, setThink] = useState(false);
   const { user, logout } = useContext(AuthContext);
+
+  console.log(`Width : ${width} & Height : ${height}`);
 
   return (
     <View style={styles.container}>
@@ -54,10 +61,46 @@ const Home = ({ navigation }) => {
           marginVertical: 20,
           borderRadius: 15,
         }}
+        onPress={apiCheck}
       >
-        <Text style={{ color: "white", textAlign: "center" }}>
-          What is hello world in programming
-        </Text>
+        <Text style={{ color: "white", textAlign: "center" }}>What is NFT</Text>
+      </TouchableOpacity>
+      {think ? (
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "white", marginLeft: 10 }}>Let me think </Text>
+          <ActivityIndicator size="small" color="white" />
+        </View>
+      ) : isMessagePresent ? (
+        <View
+          style={{
+            padding: 10,
+            borderColor: "white",
+            borderWidth: 1,
+            borderRadius: 10,
+          }}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>{message}</Text>
+        </View>
+      ) : null}
+      <TouchableOpacity
+        style={{
+          padding: 5,
+          // backgroundColor: "grey",
+          borderRadius: 5,
+          borderWidth: 2,
+          borderColor: "grey",
+        }}
+        onPress={() => {
+          logout();
+        }}
+      >
+        <Text style={{ color: "white", textAlign: "center" }}>Sign Out</Text>
       </TouchableOpacity>
     </View>
   );
